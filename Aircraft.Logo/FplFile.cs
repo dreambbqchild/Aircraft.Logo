a4ec056140a40d6ac96a318706eab887757be971
paige.rudnick <paige.rudnick@icloud.com>
2021-12-22 22:59:38 -0600

    Add project files.

---
+++ Aircraft.Logo/FplFile.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Garmin
+{
+    internal class FplFile
+    {        
+        private static readonly XmlSerializer xmlSerializer = new XmlSerializer(typeof(FlightPlan));
+
+        private readonly FlightPlan _flightPlan;
+        private readonly List<Waypoint> _waypoints = new List<Waypoint>();
+        private readonly List<RoutePoint> _routePoints = new List<RoutePoint>();
+
+        private FplFile(FlightPlan flightPlan)
+        {
+            _flightPlan = flightPlan;
+        }
+
+        public void AddPoint(double lat, double lon)
+        {
+            var identifier = $"PT{_waypoints.Count:D3}";
+            var strLat = GeographicLib.DMS.Encode(lat, 4, GeographicLib.HemisphereIndicator.Latitude, ' ').Replace(" ", string.Empty);
+            var strLon = GeographicLib.DMS.Encode(lon, 4, GeographicLib.HemisphereIndicator.Longitude, ' ').Replace(" ", string.Empty);
+            Console.Write($"{strLat}{strLon}  ");
+            _waypoints.Add(new Waypoint()
+            {
+                identifier = identifier,
+                lat = Convert.ToDecimal(lat),
+                lon = Convert.ToDecimal(lon),
+                type = WaypointType.USERWAYPOINT
+            });
+
+            _routePoints.Add(new RoutePoint()
+            {
+                waypointidentifier = identifier,
+                waypointtype = WaypointType.USERWAYPOINT
+            });
+        }
+
+        public void Save(string path)
+        {
+            _flightPlan.waypointtable = _waypoints.ToArray();
+            _flightPlan.route.routepoint = _routePoints.ToArray();
+            var xDoc = Serialize(_flightPlan);
+            xDoc.Save(path);
+        }
+
+        private static FlightPlan Deserialize(XDocument doc)
+        {
+            using (var reader = doc.Root!.CreateReader())
+                return (xmlSerializer.Deserialize(reader) as FlightPlan)!;
+        }
+
+        private static XDocument Serialize(FlightPlan value)
+        {
+            var doc = new XDocument();
+            using (var writer = doc.CreateWriter())
+                xmlSerializer.Serialize(writer, value);
+
+            return doc;
+        }
+
+        public static FplFile Load(string path)
+        {
+            var xDoc = XDocument.Load(path);
+            return new FplFile(Deserialize(xDoc));
+        }
+
+        public static FplFile New(string name)
+        {
+            var flightPlan = new FlightPlan();            
+            flightPlan.route = new Route()
+            {
+                routename = name,
+                flightplanindex = "1"
+            };
+            return new FplFile(flightPlan);
+        }
+    }
+}
---
+++ Aircraft.Logo/Program.cs
@@ -0,0 +1,66 @@
+﻿using GeographicLib;
+using Svg;
+using Svg.Pathing;
+using System.Drawing;
+
+IReadOnlyList<PointF> GetData(string svg)
+{
+    var svgDocument = SvgDocument.Open(svg)!;
+    var path = svgDocument.Children.OfType<SvgPath>().Single();
+    return path.PathData.Where(pd => pd is not SvgClosePathSegment)
+        .Select(pd => pd.End)
+        .ToArray();
+}
+
+double MilesToMeters(double miles) 
+    => miles * 1609.344;
+
+double Distance(PointF start, PointF end) 
+    => Math.Sqrt(Math.Pow(start.X - end.X, 2) + Math.Pow(start.Y - end.Y, 2));
+
+double RadiansToDegrees(double radians) 
+    => (((radians > 0 ? radians : (2 * Math.PI + radians)) * 360.0 / (2 * Math.PI)) + 90) % 360.0;
+
+string GetArgument(string arg, string? @default = null)
+{
+    var args = Environment.GetCommandLineArgs();
+    var index = Array.IndexOf(args, $"-{arg}");
+    if (index == -1)
+    {
+        if(@default is null)
+            Environment.Exit(1);
+
+        return @default;
+    }
+
+    if (index >= args.Length)
+        Environment.Exit(2);
+
+    return args[index + 1];
+}
+
+var strLat = GetArgument("lat"); //45d07'26"N
+var strLon = GetArgument("lon"); //94d19'39"W
+var svg = GetArgument("svg"); //file.svg
+var mi = Double.Parse(GetArgument("miles", "15")); //15
+
+var (lat, lon) = DMS.Decode(strLat, strLon);
+
+var data = GetData(svg);
+var yRange = data.Max(pt => pt.Y) - data.Min(pt => pt.Y);
+var fpl = Garmin.FplFile.New(Path.GetFileNameWithoutExtension(svg));
+
+fpl.AddPoint(lat, lon);
+
+for (var i = 1; i < data.Count; i++)
+{
+    var prev = data[i - 1];
+    var current = data[i];
+    var rad = Math.Atan2(current.Y - prev.Y, current.X - prev.X);
+    var deg = RadiansToDegrees(rad);
+    var len = MilesToMeters((Distance(prev, current) / yRange) * mi);
+    Geodesic.WGS84.Direct(lat, lon, deg, len, out lat, out lon);
+    fpl.AddPoint(lat, lon);
+}
+
+fpl.Save($"{Path.GetFileNameWithoutExtension(svg)}.fpl");  
